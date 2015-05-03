6baacb0fe698ff56d1595c618264138d9f28a348
Nemesis93 <mickael.leopold1992@gmail.com>
2015-05-03 16:36:48 +0200

    prepa

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head lang="FR-fr">
+    <meta charset="UTF-8">
+    <title>...</title>
+</head>
+<body>
+    <header>
+        <div>
+           <h1></h1>
+           <nav>
+               <h2>navigation principale</h2>
+               <ul>
+                   <li><a></a></li>
+               </ul>
+           </nav>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
+<?php
+
+?>
